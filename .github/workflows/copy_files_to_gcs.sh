d37ac1d5c44fa6e14e6067c0b0c2786028876181
Pavlo Myroniuk <pspos.developqkation@gmail.com>
2022-08-06 00:49:29 +0300

    ci: improve copy files to gcs script

--- .github/workflows/copy_files_to_gcs.sh
+++ .github/workflows/copy_files_to_gcs.sh
@@ -1,9 +1,18 @@
 gsutil -h "Content-Type:text/javascript" -h "Cache-Control:no-store" cp asn1.js gs://${GCP_BUCKET}
+echo "1"
 gsutil -h "Content-Type:text/javascript" -h "Cache-Control:no-store" cp base64.js gs://${GCP_BUCKET}
+echo "2"
 gsutil -h "Content-Type:text/javascript" -h "Cache-Control:no-store" cp dom.js gs://${GCP_BUCKET}
+echo "3"
 gsutil -h "Content-Type:text/javascript" -h "Cache-Control:no-store" cp hex.js gs://${GCP_BUCKET}
+echo "4"
 gsutil -h "Content-Type:text/javascript" -h "Cache-Control:no-store" cp index.js gs://${GCP_BUCKET}
+echo "5"
 gsutil -h "Content-Type:text/javascript" -h "Cache-Control:no-store" cp int10.js gs://${GCP_BUCKET}
+echo "6"
 gsutil -h "Content-Type:text/html" -h "Cache-Control:no-store" cp index.html gs://${GCP_BUCKET}
+echo "7"
 gsutil -h "Content-Type:text/css" -h "Cache-Control:no-store" cp index.css gs://${GCP_BUCKET}
+echo "8"
 gsutil -h "Content-Type:text/javascript" -h "Cache-Control:no-store" cp oids.js gs://${GCP_BUCKET}
+echo "9"
