29e8104b5f4c1056d95c3b1655ba9d1c01afbda1
Pavlo Myroniuk <pspos.developqkation@gmail.com>
2022-08-05 23:16:20 +0300

    add simple deploy ci configuration

---
+++ .github/workflows/copy_files_to_gcs.sh
@@ -0,0 +1,9 @@
+gsutil -h "Content-Type:text/javascript" -h "Cache-Control:no-store" cp asn1.js gs://${GCP_BUCKET}
+gsutil -h "Content-Type:text/javascript" -h "Cache-Control:no-store" cp base64.js gs://${GCP_BUCKET}
+gsutil -h "Content-Type:text/javascript" -h "Cache-Control:no-store" cp dom.js gs://${GCP_BUCKET}
+gsutil -h "Content-Type:text/javascript" -h "Cache-Control:no-store" cp hex.js gs://${GCP_BUCKET}
+gsutil -h "Content-Type:text/javascript" -h "Cache-Control:no-store" cp index.js gs://${GCP_BUCKET}
+gsutil -h "Content-Type:text/javascript" -h "Cache-Control:no-store" cp int10.js gs://${GCP_BUCKET}
+gsutil -h "Content-Type:text/html" -h "Cache-Control:no-store" cp index.html gs://${GCP_BUCKET}
+gsutil -h "Content-Type:text/css" -h "Cache-Control:no-store" cp index.css gs://${GCP_BUCKET}
+gsutil -h "Content-Type:text/javascript" -h "Cache-Control:no-store" cp oids.js gs://${GCP_BUCKET}
