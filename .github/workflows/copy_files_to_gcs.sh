c59a5a497612990f8812ae173c7b39901c16a78c
Pavlo Myroniuk <pspos.developqkation@gmail.com>
2022-08-06 22:37:53 +0300

    ci: now examples are also uploaded; general: improve main page

--- .github/workflows/copy_files_to_gcs.sh
+++ .github/workflows/copy_files_to_gcs.sh
@@ -7,3 +7,4 @@ gsutil -h "Content-Type:text/javascript" -h "Cache-Control:no-store" cp int10.js
 gsutil -h "Content-Type:text/html" -h "Cache-Control:no-store" cp index.html gs://${GCP_BUCKET}
 gsutil -h "Content-Type:text/css" -h "Cache-Control:no-store" cp index.css gs://${GCP_BUCKET}
 gsutil -h "Content-Type:text/javascript" -h "Cache-Control:no-store" cp oids.js gs://${GCP_BUCKET}
+gsutil -h "Content-Type:text/plain" -h "Cache-Control:no-store" cp -r examples gs://${GCP_BUCKET}
